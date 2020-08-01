8470ce3e074edd39dcbf8b8272eba82f4263b973
Yash Saraswat <saraswatyash879@gmail.com>
2020-08-01 20:40:34 +0530

    Updates

---
+++ precache-manifest.5ec7099336a9c241df6c8a0fd7237259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaddb3a53f78160bd76e847676f49af7",
+    "url": "/todolistlive/index.html"
+  },
+  {
+    "revision": "b468f758f24ac15b56da",
+    "url": "/todolistlive/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ff85cc4e3bd5bbf65c3a",
+    "url": "/todolistlive/static/css/main.86bbae23.chunk.css"
+  },
+  {
+    "revision": "b468f758f24ac15b56da",
+    "url": "/todolistlive/static/js/2.83808839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolistlive/static/js/2.83808839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff85cc4e3bd5bbf65c3a",
+    "url": "/todolistlive/static/js/main.e6943752.chunk.js"
+  },
+  {
+    "revision": "ad0e5a9697aaa892ca7b",
+    "url": "/todolistlive/static/js/runtime-main.bba19035.js"
+  }
+]);
